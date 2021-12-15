99f40f525227d0cf9f16e963db67fdd6dcfd10c6
Utsavthebug <neupaneutsav.2001@gmail.com>
2021-12-15 14:58:53 +0545

    cart functionality added

--- greatKart/settings.py
+++ greatKart/settings.py
@@ -28,7 +28,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'category',
     'accounts',
-    'store'
+    'store',
+    'carts',
 ]
 
 MIDDLEWARE = [
@@ -55,6 +56,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'category.context_processors.menu_links',
+                'carts.context_processors.counter',
             ],
         },
     },
--- greatKart/urls.py
+++ greatKart/urls.py
@@ -8,5 +8,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home,name='home-page'),
-    path('store/',include('store.urls'))
+    path('store/',include('store.urls')),
+    path('cart/',include('carts.urls')),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
