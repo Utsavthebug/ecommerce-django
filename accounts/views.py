c78fbf2ec801674d24a52826783926cdb92d80a4
Utsavthebug <neupaneutsav.2001@gmail.com>
2021-12-18 10:32:48 +0545

    user registration added

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
-# Create your views here.
+from accounts.models import Account
+from .forms import RegistrationForm
+
+def register(request):
+    form = RegistrationForm()
+    if request.method =='POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            phone_number = form.cleaned_data['phone_number']
+            password = form.cleaned_data['password']
+            username = email.split("@")[0]
+            user = Account.objects.create(first_name=first_name,last_name=last_name,email=email,username=username)
+            user.phone_number = phone_number
+            user.set_password(password)
+            user.save()
+           
+    context = {
+        'form':form
+    }
+    return render(request,'accounts/register.html',context)
+
+def login(request):
+    return render(request,'accounts/login.html')
+
+
+def logout(request):
+    return render(request,'accounts/logout.html')
+
+
